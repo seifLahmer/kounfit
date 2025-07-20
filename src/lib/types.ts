afd63593837d57c885726660831aa33a777fb7e4
Zakaria Ben Hajji <zakaria.benhajji@edu.isetcom.tn>
2025-07-20 20:08:40 +0000

    connect the app to the tha firebase , the components , the list of trait

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,14 +1,84 @@
+
+
+export type User = {
+  uid: string;
+  email: string;
+  fullName: string;
+  age?: number;
+  biologicalSex?: "male" | "female";
+  weight?: number; // in kg
+  height?: number; // in cm
+  deliveryAddress?: string;
+  region?: string;
+  activityLevel?: "sedentary" | "lightly_active" | "moderately_active" | "very_active";
+  mainGoal?: "lose_weight" | "maintain" | "gain_muscle";
+  calorieGoal?: number;
+  macroRatio?: {
+    protein: number;
+    carbs: number;
+    fat: number;
+  };
+  favoriteMealIds?: string[];
+  role: "client" | "caterer" | "admin";
+};
+
+export type Component = {
+  id?: string;
+  name: string;
+  pricePer100g: number; // in DT
+  macros: {
+    protein: number;
+    carbs: number;
+    fat: number;
+  };
+  createdBy: string; // admin UID
+};
+
 export interface Meal {
+  id?: string;
   name: string;
   description: string;
+  category: "breakfast" | "lunch" | "dinner" | "snack";
+  imageUrl: string;
+  ingredients: {
+    componentId: string;
+    grams: number;
+  }[];
   calories: number;
   macros: {
     protein: string;
     carbs: string;
     fat: string;
   };
-  ingredients?: string[];
+  price: number;
+  createdBy: string; // admin UID
+  availability: boolean;
 }
+
+export type Caterer = {
+  uid: string; // This will be the same as their auth UID
+  name: string;
+  email: string;
+};
+
+export type Order = {
+  id?: string;
+  clientId: string;
+  clientName: string;
+  clientRegion: string;
+  items: {
+    mealId: string;
+    mealName: string;
+    quantity: number;
+    unitPrice: number;
+  }[];
+  totalPrice: number;
+  status: "pending" | "in_preparation" | "delivered" | "cancelled";
+  orderDate: Date;
+  deliveryDate: Date;
+  deliveryAddress: string;
+};
+
 
 export interface MealPlan {
   breakfast: Meal;
