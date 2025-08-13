b0582625be8f60c4076d51a9a3ef0235730561c4
Zakaria Ben Hajji <zakaria.benhajji@edu.isetcom.tn>
2025-08-13 23:48:53 +0000

    studio-8662129095:~/studio{master}$ npx cap sync ios [error] The web ass

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,9 +1,10 @@
+
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'kounfit.app',
   appName: 'Kounfit',
-  webDir: '.next'
+  webDir: 'out'
 };
 
 export default config;
