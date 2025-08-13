18703b0f7807eed8a20371999f77178417ebbc95
Zakaria Ben Hajji <zakaria.benhajji@edu.isetcom.tn>
2025-08-13 23:07:11 +0000

    > Build error occurred [Error: Page "/home/meal/[id]" is missing "genera

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'kounfit.app',
   appName: 'Kounfit',
-  webDir: 'out'
+  webDir: '.next'
 };
 
 export default config;
