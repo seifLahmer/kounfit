8362360837ee61c3808fe1af2f6ef55cc06be366
Zakaria Ben Hajji <zakaria.benhajji@edu.isetcom.tn>
2025-08-14 14:41:20 +0000

    same error , u fix it before with this plan "La solution que j'ai appliq

--- next.config.ts
+++ next.config.ts
@@ -36,25 +36,6 @@ const nextConfig: NextConfig = {
       },
     ],
   },
-  webpack: (config, { isServer }) => {
-    // This is to fix the "Cannot find module './XXX.js'" error
-    // It is caused by server-side dependencies being bundled in the client-side build
-    if (!isServer) {
-        config.externals = [
-            ...config.externals,
-            '@opentelemetry/exporter-jaeger',
-            '@genkit-ai/firebase',
-        ];
-    }
-
-    // This is to fix the "require.extensions is not supported by webpack" warning
-    config.resolve.fallback = {
-      ...config.resolve.fallback,
-      'handlebars': false,
-    };
-    
-    return config;
-  },
 };
 
 export default nextConfig;
