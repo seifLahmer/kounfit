5911c77d9a299ddb50b7f5b90771b32e318d6c77
Zakaria Ben Hajji <zakaria.benhajji@edu.isetcom.tn>
2025-08-23 14:47:31 +0000

    The NextJS app server is having trouble starting. Please identify what c

---
+++ src/next.config.ts
@@ -0,0 +1,48 @@
+
+import type {NextConfig} from 'next';
+
+const nextConfig: NextConfig = {
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  productionBrowserSourceMaps: false, // Disable source maps in production
+  images: {
+    unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'unpkg.com',
+        port: '',
+        pathname: '/**',
+      },
+       {
+        protocol: "https",
+        hostname: "firebasestorage.googleapis.com",
+      },
+    ],
+  },
+   webpack: (config, { isServer }) => {
+     config.module.rules.push({
+      test: /node_modules\/handlebars\//,
+      loader: 'ignore-loader',
+    });
+    return config;
+  },
+};
+
+export default nextConfig;
