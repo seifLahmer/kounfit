705d1af4bcfc1b537eb50b518d804eed9a7b67f1
Zakaria Ben Hajji <zakaria.benhajji@edu.isetcom.tn>
2025-08-30 23:07:12 +0000

    ton dernier modication sur capacitor.config.ts et next.config.ts et .env

---
+++ src/app/next.config.ts
@@ -0,0 +1,47 @@
+
+import type {NextConfig} from 'next';
+
+const nextConfig: NextConfig = {
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  productionBrowserSourceMaps: false, // Disable source maps in production
+  images: {
+    unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'unpkg.com',
+        port: '',
+        pathname: '/**',
+      },
+       {
+        protocol: "https",
+        hostname: "firebasestorage.googleapis.com",
+      },
+    ],
+  },
+   webpack: (config, { isServer }) => {
+    if (isServer) {
+        config.externals.push('handlebars');
+    }
+    return config;
+  },
+};
+
+export default nextConfig;
