47febd7b732e62cee4959ae7c2ca97209cce241e
Zakaria Ben Hajji <zakaria.benhajji@edu.isetcom.tn>
2025-09-01 13:14:49 +0000

    "   Checking validity of types  ..Failed to compile.
    src/app/home/meal/

--- src/app/home/meal/[id]/page.tsx
+++ src/app/home/meal/[id]/page.tsx
@@ -1,6 +1,7 @@
+
 import MealDetailClient from './meal-detail-client';
 
 export default function MealDetailPage({ params }: { params: { id: string } }) {
-  // We pass the mealId to the client component, which will handle fetching.
+  // Nous passons le mealId au composant client, qui se chargera de la récupération des données.
   return <MealDetailClient mealId={params.id} />;
 }
