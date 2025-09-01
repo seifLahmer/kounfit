9a23db2d61c80243cd031e786cd1f13e560ecd40
Zakaria Ben Hajji <zakaria.benhajji@edu.isetcom.tn>
2025-09-01 15:36:00 +0000

    studio-8662129095:~/studio{master}$ npm run build
    > kounfit@0.1.0 build

--- next.config.ts
+++ next.config.ts
@@ -2,39 +2,15 @@
 import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
+  output: 'export',
   typescript: {
     ignoreBuildErrors: false,
   },
   eslint: {
     ignoreDuringBuilds: true,
   },
-  productionBrowserSourceMaps: false, // Disable source maps in production
   images: {
     unoptimized: true,
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'placehold.co',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'lh3.googleusercontent.com',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'unpkg.com',
-        port: '',
-        pathname: '/**',
-      },
-       {
-        protocol: "https",
-        hostname: "firebasestorage.googleapis.com",
-      },
-    ],
   },
 };
 
