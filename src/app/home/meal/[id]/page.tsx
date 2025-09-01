eef5482cbc000661f27b328d12c6b884168cd077
Zakaria Ben Hajji <zakaria.benhajji@edu.isetcom.tn>
2025-09-01 13:25:05 +0000

    "studio-8662129095:~/studio{master}$ npm run build
    > kounfit@0.1.0 buil

--- src/app/home/meal/[id]/page.tsx
+++ src/app/home/meal/[id]/page.tsx
@@ -2,6 +2,5 @@
 import MealDetailClient from './meal-detail-client';
 
 export default function MealDetailPage({ params }: { params: { id: string } }) {
-  // Nous passons le mealId au composant client, qui se chargera de la récupération des données.
   return <MealDetailClient mealId={params.id} />;
 }
